f2257f599cbbee920f90fa34446ee70da88d28ef
ColinUrbs <colin.urban@gmail.com>
2025-07-13 22:54:09 -0400

    version

--- modules/version.py
+++ modules/version.py
@@ -4,5 +4,5 @@ This module provides a central location for version information.
 """
 
 # Version information
-APP_VERSION = "0.5"  # Numeric version for metadata
+APP_VERSION = "0.5.1"  # Numeric version for metadata
 APP_VERSION_DISPLAY = f"v{APP_VERSION}"  # Display version for toolbar
